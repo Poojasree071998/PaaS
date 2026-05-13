023c4456aca0a81db070c182bc59d5914ea39358
Pooja <poojavenkatesan15042001@gmail.com>
2026-05-13 12:30:18 +0530

    fix: copy workspace node_modules in Dockerfile to prevent MODULE_NOT_FOUND

--- apps/api/src/middlewares/rateLimiter.ts
+++ apps/api/src/middlewares/rateLimiter.ts
@@ -1,4 +1,4 @@
-import rateLimit from 'express-rate-limit';
+import { rateLimit } from 'express-rate-limit';
 import { Request, Response } from 'express';
 
 export const standardRateLimiter = rateLimit({
